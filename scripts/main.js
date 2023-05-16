56ffbc767eeda9599aa8ee59235524eec3d50866
Peter Collingridge <peter.collingridge@nanoporetech.com>
2023-05-16 22:05:45 +0100

    Test new adding drag

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,22 @@ const { VerletPhysics2D, VerletParticle2D, VerletSpring2D } = toxi.physics2d;
 const { GravityBehavior } = toxi.physics2d.behaviors;
 const { Vec2D, Rect } = toxi.geom;
 
+const GROWTH_THRESHOLD = 100;
 const debug = 2;
-let running = true;
 
+let running = true;
 let physics;
-let cell;
+
+const drag = {
+  applyBehavior: function(p) {
+    if (p.y > height - 100) {
+      p.scaleVelocity(0.1);
+    }
+  },
+  configure: function(timeStep) {
+    this.timeStep = timeStep;
+  },
+}
 
 function setup() {
   createCanvas(800, 500);
@@ -15,18 +26,18 @@ function setup() {
   const gravity = new GravityBehavior(new Vec2D(0, 1));
   physics.addBehavior(gravity);
 
+  physics.addBehavior(drag);
+
   const bounds = new Rect(0, 0, width, height);
   physics.setWorldBounds(bounds);
 
   life = new Life(width/2, height/2, 30);
-
-  // life.addChild(0, 1);
-  // life.addChild(0, 0);
 }
 
 function draw() {
   if (running) {
     physics.update();
+    life.update();
   }
   background(255);
   life.draw();
